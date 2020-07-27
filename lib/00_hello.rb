2784675c1b16745537a8fbfb847c6515dbdb09c5
Mamitiana <mamitiana626@gmail.com>
2020-07-27 17:12:19 +0200

    ajout commentaire dans le jeu dire bjr

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,3 +1,4 @@
+#fonction dire bonjour sans saisie nom
 def say_hello(nom)
     msg = "bonjour #{nom}"
 end 
@@ -5,6 +6,7 @@ end
 puts "\nProgramme qui dit bonjour en ruby sans enter de variable"
 puts say_hello("rakoto")
 
+#fonction dire bonjour avec saisie nom
 def say_hello2(nom)
     msg = "bonjour #{nom}"
 end
@@ -14,6 +16,7 @@ puts "Saisir votre nom "
 first_name = gets.chomp
 puts say_hello2(first_name)
 
+#fonction dire bonjour avec 2 methodes differents ask_first_name et  say_hello3 avec saisie nom
 def ask_first_name(nom)
     say_hello3(nom)
 end 

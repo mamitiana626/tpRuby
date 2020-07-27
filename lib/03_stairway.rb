3dec5e5ed6754a244591b52904179638621db32d
Mamitiana <mamitiana626@gmail.com>
2020-07-27 17:26:14 +0200

    ajout commentaire dans le jeu stairway

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -6,11 +6,14 @@ nombre_de_partie = 0
 puts "//Lancement de la partie//"
 while partie == 'o'
     puts "=============================================================="
+    #lancement du dE
     puts "Le dE se lance..."
     d_random = rand(1..6)
+    #affichage resultat du dE
     puts "Le dE fait #{d_random} :)"
     case d_random
         when 5 ,6
+          #si le dE fait 5 et 6 on avance d'une marche
           puts "\n (^_^) Bravo!!! Vous avez avancer une marche"
           escalier = escalier + 1
           if escalier < 0
@@ -18,6 +21,7 @@ while partie == 'o'
           end
           puts "Vous etes sur la case : #{escalier}"
         when 1
+          #si le dE fait 1 descend une marche
           puts "\n (>_<) oh! Courage, ce n'est pas la fin du monde,  (^_^) Vous descend une marche"
           escalier = escalier - 1
           if escalier < 0
@@ -25,9 +29,11 @@ while partie == 'o'
           end
           puts "Vous etes sur la case : #{escalier}" 
         when 2,3,4
+          #si le dE fait 2,3 et 4 rien ne se passe et on reste sur la meme position
            puts "\n hahaha! :P Rien ne se passe,vous restez sur la meme position case #{escalier}"
            puts "Vous etes sur la case : #{escalier}" 
     end
+    #si on atteint la 10eme etage, affichage msg "bravo"
     if escalier == etage
         puts "\n--------||| Bravo!!! vous etes arrivE au 10eme etages |||--------\n"
         break
@@ -39,7 +45,7 @@ while partie == 'o'
 
 end
 puts "****Fin de la partie****"
-
+#affichage statistique du jeu
 puts "Nombre de la partie = #{nombre_de_partie}"
 def average_finish_time(nombre_de_partie)
     (100/nombre_de_partie)-1

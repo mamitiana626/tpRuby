1d5cce3bb0f1814f9dec76092405342a0efc2242
Mamitiana <mamitiana626@gmail.com>
2020-07-27 17:07:10 +0200

    ajout commentaire dans le jeu mot de passe

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,9 +1,11 @@
+#fonction pour cree un mot de passe
 def signup
 	puts "\nVeillez cree votre mot de passe"
     mdp=gets.chomp
     login(mdp)
 end
 
+#fonction checking mot de passe ulitisateur 
 def login(mdp)
     puts "\nVeillez entrer votre mot de passe"	
     mdp2=gets.chomp  
@@ -19,6 +21,7 @@ def login(mdp)
     end	
 end
 
+#fonction pour afficher un message de bienvenu si l'authentification est avec success
 def welcome_screen
     puts "\n\nBIENVENUE oh chers utilisateur\nComme vous ete arriver jusqu'ici je vais vous devoiler un petit secret\nje vais vais vous dire la secret de l immortaliter\nLe Yomi Yomi no Mi, ou Fruit de la Résurrection,\nc est un Fruit du Démon de type Paramecia qui permet à son utilisateur de revenir à la vie après sa mort, faisant de lui un Homme Ressuscité"
 end
